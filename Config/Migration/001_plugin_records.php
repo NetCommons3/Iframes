7fb4aac0652e70c9659dd77666de349d9f237d5d
s-nakajima <nakajimashouhei@gmail.com>
2017-01-13 17:01:54 +0900

    多言語対応 https://github.com/NetCommons3/NetCommons3/issues/770

--- Config/Migration/001_plugin_records.php
+++ Config/Migration/001_plugin_records.php
@@ -51,6 +51,7 @@ class PluginRecords extends NetCommonsMigration {
 				'type' => 1,
 				'default_action' => 'iframes/view',
 				'default_setting_action' => 'iframe_blocks/index',
+				'is_m17n' => false,
 			),
 			//英語
 			array(
@@ -61,6 +62,7 @@ class PluginRecords extends NetCommonsMigration {
 				'type' => 1,
 				'default_action' => 'iframes/view',
 				'default_setting_action' => 'iframe_blocks/index',
+				'is_m17n' => false,
 			),
 		),
 		'PluginsRole' => array(
